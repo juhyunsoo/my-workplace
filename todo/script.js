17ec1cf03924c7405ff84f311f384fc532f1643f
unknown1102 <86240866+juhyunsoo@users.noreply.github.com>
2023-08-04 16:12:14 +0900

    Add files via upload

--- todo/script.js
+++ todo/script.js
@@ -2,6 +2,8 @@
 const DB_NAME = "myDatabase";
 const VER = 1;
 const STORE_NAME = "data";
+const DATE_CONVERT = 1000 * 60 * 60 * 24;
+const CURRENT_DATE = new Date();
 
 let db;
 function openDB() {
@@ -12,6 +14,7 @@ function openDB() {
         const store = db.createObjectStore(STORE_NAME, { keyPath: "id", autoIncrement: true });
         store.createIndex("text", "text", { unique: false });
         store.createIndex("date", "date", { unique: false });
+        store.createIndex("savedDate", "savedDate", {unique : false});
     };
     
     openRequest.onsuccess = function(event) {
@@ -28,7 +31,9 @@ openDB();
 // 저장된 아이템 불러오기
 function loadItems() {
     const itemList = document.getElementById("itemList");
+    const expiredList = document.getElementById("expiredList");
     itemList.innerHTML = "";
+    expiredList.innerHTML = "";
 
     const store = db.transaction(STORE_NAME, "readonly").objectStore(STORE_NAME);
     const buffer = [];
@@ -38,16 +43,32 @@ function loadItems() {
             buffer.push(cursor.value);
             cursor.continue();
         }
-        //모든 데이터 불러온 다음 날짜순으로 정렬
+        //모든 데이터를 불러왔다면, 날짜순으로 정렬해 화면에 출력
         else {
             buffer.sort((a, b) => new Date(a.date) - new Date(b.date));
             for(const temp of buffer) {
                 const li = document.createElement("li");
-                li.textContent = `${temp.date} - ${temp.text}`;
+                //날짜계산
+                var tempTotalDate = (temp.date - temp.savedDate) / DATE_CONVERT;
+                const totalDate = Math.max(1, tempTotalDate);
+                const divisionDate = Math.max(0, Math.min((CURRENT_DATE - temp.savedDate) / DATE_CONVERT, tempTotalDate));
+                const percentDate = divisionDate / totalDate * 100;
+                //날짜 표기형식 변환
+                var tempY = temp.date.getFullYear();
+                var tempM = temp.date.getMonth() + 1;
+                var tempD = temp.date.getDate();
+                const dateToString = tempY + "년 " + tempM + "월 " + tempD + "일 "
+                //화면에 표시
+                li.textContent = `${dateToString} - ${temp.text} (${percentDate}퍼센트)`;
                 li.addEventListener("click", function() {
                     deleteItem(temp.id);
                 });
-                itemList.appendChild(li);
+                if(temp.date < CURRENT_DATE) {
+                    expiredList.appendChild(li);
+                }
+                else {
+                    itemList.appendChild(li);
+                }
             }
         }
     };
@@ -56,7 +77,8 @@ function loadItems() {
 // 아이템 저장
 document.getElementById("saveButton").addEventListener("click", function() {
     const text = document.getElementById("text").value;
-    const date = document.getElementById("datePicker").value;
+    const date = new Date(document.getElementById("datePicker").value);
+    const savedDate = CURRENT_DATE;
     if(!text) {
         return alert("할 일을 적어 주세요");
     }
@@ -64,7 +86,7 @@ document.getElementById("saveButton").addEventListener("click", function() {
         return alert("날짜를 정해 주세요");
     }
     const store = db.transaction(STORE_NAME, "readwrite").objectStore(STORE_NAME);
-    const obj = { text, date };
+    const obj = { text, date, savedDate };
     let req;
     try {
         req = store.add(obj);
